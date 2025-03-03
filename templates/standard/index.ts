4b13fb30e0dbb9abb53a421c944b8e22551485cc
Aryan75Way <aryn.75way@gmail.com>
2025-03-03 17:38:31 +0530

    integrate: swagger

--- templates/standard/index.ts
+++ templates/standard/index.ts
@@ -2,9 +2,13 @@ import express, { type Express, type Request, type Response } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import http from "http";
+import cors from "cors";
 import { loadConfig } from "./app/common/helper/config.helper";
 import errorHandler from "./app/common/middleware/error-handler.middleware";
 import routes from './app/route';
+import { initDB } from "./app/common/services/database.service";
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
 
 loadConfig();
 
@@ -16,20 +20,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors({
+  origin: '*',
+}))
 
-const initApp = async (): Promise<void> => {
-  // set base path to /api
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+/**
+ * Initializes the application.
+ *
+ * Initializes the database connection, adds routes, adds error handler, and
+ * starts the HTTP server.
+*/
+const initApp = async () => {
+  // await initDB();
+
   app.use("/api", routes);
+
+  app.use(errorHandler);
 
   app.get("/", (req: Request, res: Response) => {
     res.send({ status: "ok" });
   });
 
-  app.use(errorHandler);
+  const httpServer = http.createServer(app)
 
-  http.createServer(app).listen(port, () => {
-    console.log("Server is runnuing on port", port);
-  });
+  httpServer.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+  })
 };
 
 void initApp();
